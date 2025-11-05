1db038017265e5629ff77902a13ae334f1e8fad2
nXhermane <nxhermane@gmail.com>
2025-11-05 15:12:48 +0100

    Refactor index.ts to implement bootstrap function and integrate NutritionistController with Elysia routes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,31 @@
 import { Elysia } from "elysia";
+import { NutritionistController } from "./controller";
+import { NutritionistSchema } from "./schema";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+function bootstrap() {
+  const nutritionistController = new NutritionistController();
+  const app = new Elysia()
+    .get("/", (context) => nutritionistController.getAllNutritionists(context))
+    .post("/", (context) => nutritionistController.createNutritionist(context), {
+      body: NutritionistSchema.createNutritionistSchema
+    })
+    .get("/:id", (context) => nutritionistController.getNutritionist(context), {
+      params: NutritionistSchema.identifySchema
+    }).
+    put("/:id", context => nutritionistController.updateNutritionist(context), {
+      params: NutritionistSchema.identifySchema,
+      body: NutritionistSchema.updateNutritionistSchema
+    })
+    .delete("/:id", context => nutritionistController.deleteNutritionist(context), {
+      params: NutritionistSchema.identifySchema
+    })
+    .get('/specializations', (context) => nutritionistController.getAllSpecialization(context))
+    .listen(3000);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+  console.log(
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  );
+}
+
+bootstrap();
+
