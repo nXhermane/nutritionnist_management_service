541fa000e144f23f2407e2191e3c88fbea5730a4
nXhermane <nxhermane@gmail.com>
2025-11-05 15:05:20 +0100

    Refactor NutritionistController to implement CRUD operations with Prisma integration

--- src/controller/nutritionist.ts
+++ src/controller/nutritionist.ts
@@ -1,23 +1,105 @@
 import { Context } from "elysia";
 import { NutritionistSchema } from "../schema";
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClientKnownRequestError } from "../generated/prisma/internal/prismaNamespace";
+
 
 
 export class NutritionistController {
-   
-
-    async createNutritionist(ctx: Context<{body: typeof NutritionistSchema.createNutritionistSchema.static}>) { 
-        // TODO: add implementation
+    private readonly prisma: PrismaClient
+    constructor() {
+        this.prisma = new PrismaClient()
     }
-     async getNutritionist(ctx: Context<{ params:  typeof NutritionistSchema.identifySchema.static }>) {
-        // TODO: add implementation
-     }
-    async getAllNutritionists(ctx: Context) { 
-        // TODO: add implementation
+    async createNutritionist({ body, status }: Context<{ body: typeof NutritionistSchema.createNutritionistSchema.static }>) {
+        try {
+            const nutritonist = await this.prisma.nutritionist.create({
+                data: {
+                    email: body.email,
+                    firstName: body.firstName,
+                    lastName: body.lastName,
+                    phoneNumber: body.phoneNumber,
+                    address: body.address,
+                    specializations: {
+                        connect: body.specializationIds?.map(id => ({ id })),
+                        create: body.specializations
+                    }
+                }
+            })
+            return status("Created", nutritonist)
+        } catch (error: any) {
+            if (error instanceof PrismaClientKnownRequestError && error.code === 'P2002') {
+                return status(409, { error: error.message });
+            }
+            return this.handleError(error, status)
+        }
     }
-    async updateNutritionist(ctx: Context<{body: typeof NutritionistSchema.updateNutritionistSchema, params: typeof NutritionistSchema.identifySchema.static}>) { 
-        // TODO: add implementation
+    async getNutritionist({ params, status }: Context<{ params: typeof NutritionistSchema.identifySchema.static }>) {
+        try {
+            const nutritionist = await this.prisma.nutritionist.findFirst({
+                where: {
+                    id: params.id
+                }
+            })
+            if (nutritionist === null) {
+                return status("Not Found", { error: `Nutritionist with id ${params.id} not found.` })
+            }
+            return status("OK", nutritionist)
+        } catch (error) {
+            return this.handleError(error, status)
+        }
     }
-    async deleteNutritionist(ctx: Context<{params: typeof NutritionistSchema.identifySchema}>) { 
-        // TODO: add implementation
+    async getNutritionistWithSpecialization({ params, status }: Context<{ params: typeof NutritionistSchema.identifySchema.static }>) {
+        try {
+            const nutritionistWithSpecialization = await this.prisma.nutritionist.findFirst({
+                where: {
+                    id: params.id,
+                },
+                include: {
+                    specializations: true,
+                }
+            })
+            if (nutritionistWithSpecialization === null) {
+                 return status("Not Found", { error: `Nutritionist with id ${params.id} not found.` })
+            }
+            return status("OK", nutritionistWithSpecialization)
+        } catch (error) {
+            return this.handleError(error, status)
+        }
+    }
+    async getAllNutritionists({ status }: Context) {
+        try {
+            const nutritionists = await this.prisma.nutritionist.findMany()
+            return status("OK", nutritionists)
+        } catch (error) {
+            return this.handleError(error, status)
+        }
+    }
+    async updateNutritionist({ params, body, status }: Context<{ body: typeof NutritionistSchema.updateNutritionistSchema.static, params: typeof NutritionistSchema.identifySchema.static }>) {
+        try {
+            const nutritionist = await this.prisma.nutritionist.update({
+                where: {
+                    id: params.id
+                },
+                data: body
+            })
+            return status("OK", nutritionist)
+        } catch (error) {
+            return this.handleError(error, status)
+        }
+    }
+    async deleteNutritionist({ params, status }: Context<{ params: typeof NutritionistSchema.identifySchema }>) {
+        try {
+           await this.prisma.nutritionist.delete({
+                where: {
+                    id: params.id
+                }
+            })
+            return status ("OK",{message: "Nutritionsit is sucessful delete."})
+        } catch (error) {
+            return this.handleError(error, status)
+        }
+    }
+    private handleError(error: any, status: Context['status']) {
+        return status(500, { error: error['message'] || JSON.stringify(error) })
     }
 }
